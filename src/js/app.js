c2588835d75730c38a8b85bf49e53c01bbb4ce6e
wulanratnamayang <wulanratnamayang@users.noreply.github.com>
2021-01-02 20:34:45 +0700

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,7 @@ import "./menu-toggler";
 import "./modal";
 import "./anchor-link-smooth";
 import "./carousel";
+import "./accordion";
+import "./slider";
+import "./shoppingCart";
+import "./shippingDetail";
